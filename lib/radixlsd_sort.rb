94e873e3d6fe0d5fd7466e00808c56aaf8d9c514
Kieran Gibb <kierangibb@Kierans-MBP.default>
2017-03-04 00:08:29 +0000

    Added Radix Sort - Least Significant Digit

---
+++ lib/radixlsd_sort.rb
@@ -0,0 +1,23 @@
+require 'benchmark'
+require 'pry'
+
+puts Benchmark.measure {
+
+  array = []
+  100000.times { array << rand(100000) }
+
+  mod = 10
+  n = 1
+  loop do
+    buckets = { 0 => [], 1 => [], 2 => [], 3 => [], 4 => [], 5 => [], 6 => [], 7 => [], 8 => [], 9 => [] }
+    (0...array.length - 1).each do |i|
+      position = (array[i] % mod) / n
+      buckets[position] << array[i]
+    end
+
+    array = buckets.values.flatten
+    break if array.max / mod <= 1
+    mod *= 10
+    n *= 10
+  end
+}
